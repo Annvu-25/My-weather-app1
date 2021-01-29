ac2a6774adfa1881df4bde002cee09bff7904492
Annvu-25 <nylandervujovic@gmail.com>
2021-01-29 17:16:52 +0100

    Moved sandbox project to Github

---
+++ script.js
@@ -0,0 +1,70 @@
+let timeElement = document.querySelector("#current-time");
+let nowTime = new Date();
+let hours = nowTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = nowTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+timeElement.innerHTML = `${hours}:${minutes}`;
+
+let dateElement = document.querySelector("#current-date");
+let nowDate = new Date();
+let day = nowDate.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+dateElement.innerHTML = `${days[day]}`;
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function search(city) {
+  let apiKey = "33c546775229a1fee66d0c638aa516ca";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city").value;
+  search(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+search("New York");
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelcius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", convertToCelcius);
