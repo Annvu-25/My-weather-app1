a97fd39378048aae66f89dbbdd87bb4173d439eb
Annvu-25 <nylandervujovic@gmail.com>
2021-02-10 12:40:34 +0100

    Updated icon with JS

--- src/script.js
+++ src/script.js
@@ -30,14 +30,20 @@ dateElement.innerHTML = `${days[day]}`;
 function showTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
+  response.data.main.temp
   );
 }
+
+function showIcon(response) {
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute("img", ) = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+}
+
 
 function search(city) {
   let apiKey = "33c546775229a1fee66d0c638aa516ca";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
+  axios.get(apiUrl).then(showTemperature, showIcon);
 }
 
 function handleSubmit(event) {
