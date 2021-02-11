97508ba2e416db2a161f6bea8541b69252dfa6d1
Annvu-25 <nylandervujovic@gmail.com>
2021-02-11 16:31:20 +0100

    Finished forecast for night temperature in JS

--- src/script.js
+++ src/script.js
@@ -32,16 +32,30 @@ function showTemperature(response) {
 
   celciusTemperature = response.data.main.temp;
 
-  document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature);
+  document.querySelector("#temperature").innerHTML = Math.round(celciusTemperature);
 
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.list[3];
+  
+  forecastElement.innerHTML = `
+    <span class="all">
+    <i class="far fa-moon"></i>
+    <strong> ${Math.round(forecast.main.temp_min)}°C</strong>
+    </span>
+  `;
+} 
+
 function search(city) {
   let apiKey = "33c546775229a1fee66d0c638aa516ca";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -53,7 +67,7 @@ function handleSubmit(event) {
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+  let fahrenheitTemperature = (celciusTemperature * 9 / 5) + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);   
 }
 
@@ -63,7 +77,7 @@ let temperatureElement = document.querySelector("#temperature");
 temperatureElement.innerHTML = Math.round(celciusTemperature);
 }
 
-let celsiusTemperature = null;
+let celciusTemperature = null;
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
